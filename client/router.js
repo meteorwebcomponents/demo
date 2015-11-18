2568386a71d00a2b7fef57eea97dab5d6671f17b
HedCET <linto.cet@gmail.com>
2015-11-19 05:11:48 +0530

    mwc-card-router added

--- client/router.js
+++ client/router.js
@@ -14,6 +14,18 @@ FlowRouter.route("/", {
         mwcLayout.render("profile", {
             img: "img-test",
             text: "text-test"
-        })
+        });
+    }
+});
+
+FlowRouter.route("/sign-in", {
+    name: "sign-in",
+    action: function(p, q) {
+        console.log("@sign-in");
+
+        mwcLayout.render("sign-in", {
+            body_A: "mwc-card-route-body-1",
+            body_Z: "mwc-card-route-body-2"
+        });
     }
 });
